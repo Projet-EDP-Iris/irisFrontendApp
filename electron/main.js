c5ab6c9c8d1c9475ac75db3a84324b9ae35518d0
Jayzheh <danbayan@protonmail.com>
2026-03-18 15:49:33 +0100

    adjusted workflow

--- electron/main.js
+++ electron/main.js
@@ -1,5 +1,9 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+import { app, BrowserWindow } from 'electron'
+import { fileURLToPath } from 'url'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const isDev = process.env.NODE_ENV === 'development'
 
